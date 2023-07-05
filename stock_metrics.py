28fc406f08c72720cf38690a58a9f1b364f3a037
Vladimir <vkadnikov92@gmail.com>
2023-07-05 14:28:40 +0400

    Fix period of data  approximation

--- stock_metrics.py
+++ stock_metrics.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import streamlit as st
 import datetime
 
+
 st.write("### Quick app for importing financial graphs for a chosen company")
 st.write("")
 st.write("")
@@ -9,19 +10,20 @@ st.write("")
 st.write("")
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-st.sidebar.header('User Input Parameters')
+st.sidebar.header('Input Parameters')
 ticker_input = st.sidebar.text_input('You can choose any ticker symbol', 'AAPL')
 ticker = yf.Ticker(ticker_input)
 company_name = ticker.info["longName"]
 st.sidebar.write(f'You chose ticker of {company_name}')
-period = st.sidebar.selectbox('Period for fin data aproximation',('1d','1mo', '1y'))
+period_select = st.sidebar.selectbox('Period for fin data aproximation',('1d', '1wk', '1mo', '3mo'))
+period = str(period_select)
 default_start = datetime.date(2010, 1, 1)
 default_end = datetime.date.today()
 start = st.sidebar.date_input(label='Start date of analysis', value=default_start)
 end = st.sidebar.date_input(label='End date of analysis', value=default_end)
 
 #get the historical prices for this ticker
-tickerDf = ticker.history(period=period, start=start, end=end)
+tickerDf = ticker.history(start=start, end=end, interval=period)
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.write(f'###### Stock closing price of {company_name}')
